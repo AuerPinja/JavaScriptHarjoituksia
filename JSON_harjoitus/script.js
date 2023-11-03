1c0cf4a6fd397c6f4cb1bd6359fd86819f8aa9be
Pinja Auer <pinskumelissa@gmail.com>
2023-11-03 10:39:15 +0200

    json harjoitus

---
+++ JSON_harjoitus/script.js
@@ -0,0 +1,20 @@
+let nimi = "Pinja";
+let ika = 27;
+let vuositulot = 0;
+
+let taulukko = `<table>
+<tbody>
+<tr>
+<td>Nimi</td>
+<td>Ikä</td>
+<td>Vuositulot</td>
+</tr>
+<tr>
+<td>${nimi}</td>
+<td>${ika}</td>
+<td>${vuositulot}</td>
+</tr>
+</tbody>
+</table>`
+
+document.write(taulukko);
